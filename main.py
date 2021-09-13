c4ffe26c98f9520486e5345b599bdfe0a6c4df2c
Brendan Griffen <brendan.f.griffen@gmail.com>
2021-09-14 01:00:22 +1000

    added nlp option

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import ezpubmed as ez
+import utils_nlp
 
 if __name__ == '__main__':
     p = ez.EzPubMed()
     p.update_db()
+
+    # load zotero papers
+    dfz = utils_nlp.initialize_zotero_library("Specific.json")
+    dfv = utils_nlp.generate_zotero_embeddings(df)
+    embedding = utils_nlp.calculate_embedding(dfv)
